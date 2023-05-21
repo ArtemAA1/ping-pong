ce74d6b3e89883d12f22f26ad2707321507a9358
ArtemAA1 <email.art@mail.ru>
2023-05-21 17:41:22 +0300

    add new rules

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ win_widht = 700
 win_height = 500
 back = (200, 50, 70)
 window = display.set_mode((700,500))
-display.set_caption('Лабиринт')
+display.set_caption('Пинг-Понг')
 window.fill(back)
 
 speed_x = 3
@@ -15,14 +15,18 @@ finish = False
 clock = time.Clock()
 FPS = 60 
 
-# font.init()
-# font = font.Font(None,70)
-# win = font.render("YOU WIN!",True,(255,215,0))
-# lose = font.render('BRUH YOU LOSE!',True,(180,0,0))
+point1 = 0
+point2 = 0
 
-# mixer.init()
-# mixer.music.load('jungles.ogg')
-# mixer.music.play()
+font.init()
+font1 = font.Font(None,35)
+score1 = font1.render("Баллы1: " + str(point1) ,True,(0,0,0))
+score2 = font1.render("Баллы2: " + str(point2) ,True,(0,0,0))
+lose1 = font1.render("Игрок 1 проиграл",True,(0,0,0))
+lose2 = font1.render("Игрок 2 проиграл",True,(0,0,0))
+
+ 
+
 
 class GameSprite(sprite.Sprite):
     def __init__(self,player_image,player_x,player_y,player_speed, player_width, player_height):
@@ -51,13 +55,27 @@ class Player(GameSprite):
 
 player1 = Player('forpingpong.png',10,200,4, 30, 100)
 player2 = Player('forpingpong.png',660,200,4, 30, 100)
-ball = GameSprite('circle2-transformed.png',220, 320,4,70,70)
+ball = GameSprite('circle.png',220, 320,4,70,70)
+
+wait = 0
 
 while run:
+    if wait == 0:
+        wait = 300
+        if speed_x > 0:
+            speed_x += 1
+        else:
+            speed_x -= 1
+        if speed_y > 0:
+            speed_y += 1
+        else:
+            speed_y -= 1
+    else:
+        wait -= 1
     for e in event.get():
         if e.type == QUIT:
             run  = False
-    
+
     if not finish:
         window.fill(back)
         player1.update_left()
@@ -67,15 +85,35 @@ while run:
         ball.rect.y += speed_y
 
         if sprite.collide_rect(player1,ball) or sprite.collide_rect(player2,ball):
-            speed_x = speed_x * -1
+            speed_x *= -1
         
         if ball.rect.y > win_height-70 or ball.rect.y < 0:
-            speed_y = speed_y * -1
-        
+            speed_y *= -1 
+
+        if ball.rect.x < 0:
+            finish = True
+            point2 += 1
+            score2 = font1.render("Баллы2: " + str(point2) ,True,(0,0,0))
+            time.delay(1000)
+
+        if ball.rect.x > win_widht:
+            finish = True
+            point1 += 1 
+            score1 = font1.render("Баллы1: " + str(point1) ,True,(0,0,0))
+            time.delay(1000)
+
+        window.blit(score1,(10,0))
+        window.blit(score2,(550,0))
 
         player1.reset()  
         player2.reset() 
         ball.reset()
 
+    else: 
+        finish = False
+        speed_x = 3
+        speed_y = 3
+        ball = GameSprite('circle.png', 220,320,4,70,70)
+        
     display.update()
-    clock.tick(FPS)
+    clock.tick(FPS) 
